edf143a246d3a80009875fab29fa7fce6a61f6ea
unknown <mombellitic@gmail.com>
2023-01-03 20:10:37 -0300

    Avalith

---
+++ JavaScript/index.js
@@ -0,0 +1,24 @@
+alert('Bienvenido a JavaScript y React')
+
+const myFirstVariable = 'Hello World';
+const numberAsString = '10';
+const numberAsNumber = 10;
+const boolean = true;
+const nullValue = null;
+const undefinedValue = undefined;
+
+console.log(myFirstVariable);
+console.log(typeof myFirstVariable);
+
+const myFirstArray = [1, 2, 3, 4, 5];
+const mySecondArray = ['a', 'b', 'c', 'd', 'e'];
+
+const lengthOfFirstArray = myFirstArray.length;
+console.log(lengthOfFirstArray);
+console.log(myFirstArray.length);
+
+const myFistObject = {
+    name: 'Juan',
+    age: 30,
+}
+
